8badc7fd0cfad84076afa9e43f3096db9e41ca79
John Lee <john.lee@sv.cmu.edu>
2012-09-19 21:57:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashiru::Application.config.secret_token = 'ed289580eb271ae37907ef55afc662af5458207f1c06b95e12247f6e15f6b08a036086dd93d06d55f712b1e1d0844a4a83193cbbd0ab49c9caedab8f19f46080'
